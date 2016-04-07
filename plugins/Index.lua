4df09b2c6b8f9585e1b9cd76256156644f77fd6e
Poyer <NapsonPoyer@gmail.com>
2016-04-07 18:12:17 +0430

    Update Index.lua

--- plugins/Index.lua
+++ plugins/Index.lua
@@ -1,13 +1,13 @@
 do
 
 function run(msg, matches)
-  return 'IndexTG Bot V2.1 Supergroups\nAn advanced Administration bot based on yagop/telegram-bot' 
+  return 'Anchor Bot V.1 Supergroups\nAn advanced Administration bot based on yagop/telegram-bot' 
   
 end
 
 return {
   patterns = {
-    "^#index$"
+    "^#anchor$"
   }, 
   run = run 
 }
